693804bd408d5d655cda950aa377d42625be8859
Victor Bilyk <victorbilyk@gmail.com>
2014-02-03 15:12:15 +0700

    bash settings

---
+++ .settings.bash
@@ -0,0 +1,26 @@
+function prompt {
+  local BLACK="\[\033[0;30m\]"
+  local BLACKBOLD="\[\033[1;30m\]"
+  local RED="\[\033[0;31m\]"
+  local REDBOLD="\[\033[1;31m\]"
+  local GREEN="\[\033[0;32m\]"
+  local GREENBOLD="\[\033[1;32m\]"
+  local YELLOW="\[\033[0;33m\]"
+  local YELLOWBOLD="\[\033[1;33m\]"
+  local BLUE="\[\033[0;34m\]"
+  local BLUEBOLD="\[\033[1;34m\]"
+  local PURPLE="\[\033[0;35m\]"
+  local PURPLEBOLD="\[\033[1;35m\]"
+  local CYAN="\[\033[0;36m\]"
+  local CYANBOLD="\[\033[1;36m\]"
+  local WHITE="\[\033[0;37m\]"
+  local WHITEBOLD="\[\033[1;37m\]"
+export PS1="$GREENBOLD\u@\h\[\033[00m\]:$BLUEBOLD\w\[\033[00m\] \\$ "
+}
+prompt
+
+#alias ls='ls --color=auto'
+export CLICOLOR=1
+alias grep='grep --colour=auto'
+
+alias r="rails"
