f790f2a2b47155bfede69105ba49c949d4078506
Victor Bilyk <victorbilyk@gmail.com>
2014-10-04 13:08:41 +0000

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -s $DIR/.vim ~/.vim
+ln -s $DIR/.vimrc ~/.vimrc
+ln -s $DIR/.git ~/.git
+ln -s $DIR/.gitconfig ~/.gitconfig
+ln -s $DIR/.settings.bash ~/.settings.bash
